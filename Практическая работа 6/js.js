462222928e3db43b5ded645601e8653a210960f3
Kolians <38135954+Kolians@users.noreply.github.com>
2018-04-06 22:41:42 +0400

    Практическая работа 6

---
+++ Практическая работа 6/js.js
@@ -0,0 +1,22 @@
+class User {
+    constructor(name, mail, password, tel) {
+    this.name = name;
+    this.mail = mail;
+    this.password = 2007-2007;
+    this.tel = 89892232145;
+    console.log(' ваше логин: ' + this.name  + ',' + ' Ваш mail ' + this.mail  +  ',' + ' пароль: ' + this.password + ',' + ' номер телефона: ' + this.tel  );
+  }
+            
+}
+class Admin extends User{
+
+}
+
+let user = new User();
+let admin = new Admin();
+
+console.log(user);
+console.log(admin);
+
+    User=null;
+    user=null;
